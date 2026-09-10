0788e622b1be9bfcc97b2a2f633f73273e99e9a2
cesializarbe-sys <cesia.lizarbe@vallegrande.edu.pe>
2026-09-10 17:33:14 -0500

    Estructura inicial del backend analitico con conexion a MongoDB

---
+++ app/repositories/mongodb/sales_repository.py
@@ -0,0 +1,7 @@
+from app.database.mongodb import get_database
+class SalesRepository:
+
+    def get_all_sales(self):
+        db = get_database()
+        collection = db["sales"]
+        return list(collection.find())
